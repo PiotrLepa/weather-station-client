b6db84a63b35fa31a1539b537d34fc023f7d6831
Piotr Łępa <piotr.lepa@altconnect.pl>
2020-11-08 14:04:08 +0100

    fix timezone in date parser

--- lib/core/presentation/date_time/date_time_parser.dart
+++ lib/core/presentation/date_time/date_time_parser.dart
@@ -3,7 +3,6 @@ import 'package:injectable/injectable.dart';
 @lazySingleton
 class DateTimeParser {
   DateTime fromNetwork(String dateToParse) {
-    final date = DateTime.parse(dateToParse).toLocal();
-    return date.subtract(date.timeZoneOffset);
+    return DateTime.parse('${dateToParse}Z').toLocal();
   }
 }
