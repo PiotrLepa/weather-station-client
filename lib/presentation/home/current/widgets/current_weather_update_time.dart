b7e9c44923a468b51056a5372f97605edec1dc7a
Piotr Łępa <piotr.lepa@altconnect.pl>
2020-11-29 23:34:30 +0100

    Refresh last update time periodically

--- lib/presentation/home/current/widgets/current_weather_update_time.dart
+++ lib/presentation/home/current/widgets/current_weather_update_time.dart
@@ -1,25 +1,49 @@
-import 'package:auto_localized/auto_localized.dart';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_station/core/injection/injection.dart';
 import 'package:weather_station/core/presentation/language/strings.al.dart';
 import 'package:weather_station/core/presentation/theme/theme_provider.dart';
-import 'package:weather_station/core/presentation/util/plural_util.dart';
 import 'package:weather_station/core/presentation/widgets/common/dimens.dart';
 import 'package:weather_station/core/presentation/widgets/common/progress_button.dart';
 import 'package:weather_station/domain/bloc/current_weather/current_weather_bloc.dart';
+import 'package:weather_station/presentation/home/current/util/update_time_formatter.dart';
 
-class CurrentWeatherUpdateTime extends StatelessWidget {
-  final pluralUtil = getIt<PluralUtil>();
-
+class CurrentWeatherUpdateTime extends StatefulWidget {
   final DateTime lastUpdateTime;
   final bool refreshLoading;
 
-  CurrentWeatherUpdateTime({
+  const CurrentWeatherUpdateTime({
     Key key,
     @required this.lastUpdateTime,
     @required this.refreshLoading,
   }) : super(key: key);
+
+  @override
+  _CurrentWeatherUpdateTimeState createState() =>
+      _CurrentWeatherUpdateTimeState();
+}
+
+class _CurrentWeatherUpdateTimeState extends State<CurrentWeatherUpdateTime> {
+  final _updateTimeFormatter = getIt<UpdateTimeFormatter>();
+
+  Timer _timer;
+
+  @override
+  void initState() {
+    _timer = Timer.periodic(const Duration(seconds: 30), (timer) {
+      // Rebuild widget to update last update time
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    return super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,27 +59,30 @@ class CurrentWeatherUpdateTime extends StatelessWidget {
       child: Column(
         children: [
           RichText(
-            text:
-                TextSpan(style: DefaultTextStyle.of(context).style, children: [
-              TextSpan(
-                text: Strings.weatherUpdateTime.get(context),
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: scaleText(17),
-                  color: ThemeProvider.of(context).textColor,
+            text: TextSpan(
+              style: DefaultTextStyle.of(context).style,
+              children: [
+                TextSpan(
+                  text: Strings.weatherUpdateTime.get(context),
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: scaleText(17),
+                    color: ThemeProvider.of(context).textColor,
+                  ),
                 ),
-              ),
-              TextSpan(
-                text: _formatLastUpdateTime(context, lastUpdateTime),
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: scaleText(14),
-                  color: ThemeProvider
-                      .of(context)
-                      .textColorLight,
+                TextSpan(
+                  text: _updateTimeFormatter.format(
+                    context,
+                    widget.lastUpdateTime,
+                  ),
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: scaleText(14),
+                    color: ThemeProvider.of(context).textColorLight,
+                  ),
                 ),
-              ),
-            ]),
+              ],
+            ),
           ),
           const Divider(
             height: 24,
@@ -71,7 +98,7 @@ class CurrentWeatherUpdateTime extends StatelessWidget {
                       .read<CurrentWeatherBloc>()
                       .add(const RefreshPressed());
                 },
-                loading: refreshLoading,
+                loading: widget.refreshLoading,
                 backgroundColor: ThemeProvider
                     .of(context)
                     .primaryColorLight,
@@ -88,41 +115,6 @@ class CurrentWeatherUpdateTime extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-
-  String _formatLastUpdateTime(BuildContext context, DateTime lastUpdateTime) {
-    final difference = DateTime.now().difference(lastUpdateTime);
-    final agoString = Strings.updateTimeAgo.get(context);
-
-    if (difference.inDays >= 1) {
-      return '${Strings.updateTimeMoreThanDay.get(context)} $agoString';
-    } else if (difference.inHours >= 1) {
-      final hoursForm = _getHoursForm(difference.inHours).get(context);
-      return '${difference.inHours} $hoursForm $agoString';
-    } else if (difference.inMinutes == 0) {
-      return Strings.updateTimeNow.get(context);
-    } else {
-      final minutesForm = _getMinutesForm(difference.inMinutes).get(context);
-      return '${difference.inMinutes} $minutesForm $agoString';
-    }
-  }
-
-  PlainLocalizedString _getMinutesForm(int minutes) {
-    return pluralUtil.applyPlurals(
-      quantity: minutes,
-      one: Strings.pluralMinutesOne,
-      few: Strings.pluralMinutesFew,
-      many: Strings.pluralMinutesMany,
-    );
-  }
-
-  PlainLocalizedString _getHoursForm(int hours) {
-    return pluralUtil.applyPlurals(
-      quantity: hours,
-      one: Strings.pluralHoursOne,
-      few: Strings.pluralHoursFew,
-      many: Strings.pluralHoursMany,
     );
   }
 }
