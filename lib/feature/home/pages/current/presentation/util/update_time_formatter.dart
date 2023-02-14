515434d9ebfedac5e1bae482c0cd5480f0f334e2
Piotr Łępa <piotrlepadev@gmail.com>
2023-02-14 17:51:31 +0100

    Fix translating plurals.

--- lib/feature/home/pages/current/presentation/util/update_time_formatter.dart
+++ lib/feature/home/pages/current/presentation/util/update_time_formatter.dart
@@ -4,23 +4,17 @@ import 'package:weather_station_client/presentation/extensions.dart';
 
 @lazySingleton
 class UpdateTimeFormatter {
-
   String format(BuildContext context, DateTime lastUpdateTime) {
-    final difference =
-        Duration(minutes: 0); //DateTime.now().difference(lastUpdateTime);
-    final agoLabel = context.strings.agoLabel;
+    final difference = DateTime.now().difference(lastUpdateTime);
 
     if (difference.inDays >= 1) {
-      return "${context.strings.updateTimeOverDay} $agoLabel";
+      return context.strings.updateTimeOverDayAgo;
     } else if (difference.inHours >= 1) {
-      final hoursLabel = context.strings.hours(difference.inHours);
-      return '${difference.inHours} $hoursLabel $agoLabel';
-    } else if (difference.inMinutes == 0) {
-      return context.strings.updateTimeNow;
+      return context.strings.hoursAgo(difference.inHours);
+    } else if (difference.inMinutes >= 1) {
+      return context.strings.minutesAgo(difference.inMinutes);
     } else {
-      final minutesLabel = context.strings.minutes(difference.inMinutes);
-      return '${difference.inMinutes} $minutesLabel $agoLabel';
+      return context.strings.updateTimeNow;
     }
   }
-
 }
