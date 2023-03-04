37ee20f79281cb73b8770b4c8127a6e58aeb76b5
Piotr Łępa <piotrlepadev@gmail.com>
2023-03-04 14:17:15 +0100

    Add bugs - all days on calendar available

--- lib/feature/home/pages/hourly/presentation/hourly_weather_day_picker.dart
+++ lib/feature/home/pages/hourly/presentation/hourly_weather_day_picker.dart
@@ -19,8 +19,4 @@ Future<void> showHourlyWeatherDayPicker(
   }
 }
 
-bool _isDateAvailable(List<DateTime> availableDays, DateTime date) =>
-    availableDays.any((availableDay) =>
-        availableDay.year == date.year &&
-        availableDay.month == date.month &&
-        availableDay.day == date.day);
+bool _isDateAvailable(List<DateTime> availableDays, DateTime date) => true;
