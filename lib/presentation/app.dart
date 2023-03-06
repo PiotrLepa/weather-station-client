c20fa1ac078204bffc7d7cf65ddb3037ab2935ff
Piotr Łępa <piotrlepadev@gmail.com>
2023-03-06 21:41:49 +0100

    Set web page title

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:weather_station_client/di/dependency_injection.dart';
 import 'package:weather_station_client/feature/home/parent/bloc/home_pages_bloc.dart';
+import 'package:weather_station_client/presentation/extensions.dart';
 import 'package:weather_station_client/presentation/router/app_router.gr.dart';
 import 'package:weather_station_client/presentation/theme/theme_provider.dart';
 
@@ -16,6 +17,7 @@ class App extends StatelessWidget {
     return BlocProvider(
       create: (_) => getIt<HomePagesBloc>(),
       child: MaterialApp.router(
+        onGenerateTitle: (context) => context.strings.appName,
         localizationsDelegates: AppLocalizations.localizationsDelegates,
         supportedLocales: AppLocalizations.supportedLocales,
         routerDelegate: _appRouter.delegate(),
